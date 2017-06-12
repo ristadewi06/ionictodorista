8ff183e9b350031f656e1019ddfe9cafae4037de
Rista Dewi Damayanti <ristadewi06@gmail.com>
2017-06-12 09:02:40 +0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { HomePage } from '../pages/home/home';
+ 
+@Component({
+  template: `<ion-nav [root]="rootPage"></ion-nav>`
+})
+export class MyApp {
+  rootPage = HomePage;
+ 
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+    });
+  }
+}
